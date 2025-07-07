8a2f3fedf89232548803fd523c2b456cb37de996
manjuvani <manjuvani5699@gmail.com>
2025-07-07 15:36:13 +0530

    new

--- app.js
+++ app.js
@@ -1,69 +1,51 @@
-const express=require('express')
-const app=express();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config');
-const bodyparser=require('body-parser')
-const morgan=require('morgan')
-const mongoose=require('mongoose')
-
-const api=process.env.API_URL
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
 
-//middle ware 
-// installed using npm body-parser
-app.use(bodyparser.json())
-app.use(morgan('tiny'))
+app.use(cors());
+app.options('*', cors())
 
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(authJwt());
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use(errorHandler);
 
-// Mongoose Schema
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
-const productSchema=mongoose.Schema({
-    name:String,
-    Image:String,
-    countInStock:{
-        type:Number,
-        require:true
-    }
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
 })
-
-// Models
-
-const Product=mongoose.model('Product',productSchema)
-
-app.get(`${api}/products`,async(req,res)=>{
-    const productList= await Product.find();
-    if(!productList){
-        res.status(500).json({success:false})
-    }
-    res.send(productList)
-})
-
-
-app.post(`${api}/products`,(req,res)=>{
-    const product=new Product({
-        name:req.body.name,
-        Image:req.body.Image,
-        countInStock:req.body.countInStock
-    })
-    product.save()
-    .then((createdProduct=>{
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error:err,
-            success:false
-        })
-    })
-})
-
-mongoose.connect(process.env.CONNECTION_STRING)
 .then(()=>{
-    console.log("Connected to Database...")
+    console.log('Database Connection is ready...')
 })
-.catch((err)=>{
+.catch((err)=> {
     console.log(err);
 })
 
-app.listen(3000,()=>{
-    console.log("server listening to port http://localhost:3000")
-    
+//Server
+app.listen(3000, ()=>{
+
+    console.log('server is running http://localhost:3000');
 })
